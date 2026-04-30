2f784d8d627a0da1aa80cddce5ead39b606275d7
xiao-taotang <2836735700@qq.com>
2026-04-30 16:13:28 +0800

    fix: 添加显式等待解决CI环境元素定位问题

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,8 +1,14 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class CartPage:
     def __init__(self, driver):
         self.driver = driver
 
     def click_checkout(self):
-        self.driver.find_element(By.ID, "checkout").click()  # 结算
+        checkout_btn = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.ID,"checkout"))
+        )
+        checkout_btn.click()             # 结算
--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,10 +1,15 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class CheckoutPage:
     def __init__(self,driver):
         self.driver = driver
 
     def fill_info(self,first_name,last_name,postal_code):
+        WebDriverWait(self.driver,10).until(
+            EC.presence_of_element_located((By.ID,"first-name"))
+        )
         self.driver.find_element(By.ID, "first-name").send_keys(first_name)        # 填写信息
         self.driver.find_element(By.ID, "last-name").send_keys(last_name)
         self.driver.find_element(By.ID, "postal-code").send_keys(postal_code)
